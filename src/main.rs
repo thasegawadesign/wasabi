4040402000beb1cb3b74d06ee6d8de247d66627d
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-12-05 09:27:52 +0900

    p67

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 #![no_std]
 #![no_main]
+#![feature(offset_of)]
 
-#[no_mangle]
-fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
-    let efi_graphics_output_protocol = locate_graphics_protocol(efi_system_table).unwrap();
-    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
-    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
-    let vram = unsafe {
-        slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
-    };
-    for e in vram {
-        *e = 0xffffff;
-    }
-    // println!("Hello, world!");
-    loop {}
+use core::mem:offset_of;
+use core::mem::size_of;
+use core::panic::PanicInfo;
+use core::ptr::null_mut;
+use core::slice;
+
+type EfiVoid = u8;
+type EfiHandle = u64;
+type Result<T> = core::result::Result<T, &'static str>;
+
+#[repr(C)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct EfiGuid {
+    data1: u32,
+    data2: u16,
+    data3: u16,
+    data4: [u8; 8],
+}
+
+const EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID: EfiGuid = EfiGuid {
+    data1: 0x9042a9de,
+    data2: 0x23dc,
+    data3: 0x4a38,
+    data4: [0x96, 0xfb, 0x7a, 0xde, 0xd0, 0x80, 0x51, 0x6a],
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+#[must_use]
+#[repr(u64)]
+enum EfiStatus {
+    Success = 0,
 }
 
 #[repr(C)]
@@ -34,28 +53,16 @@ struct EfiSystemTable {
 }
 const _: () = assert!(offset_of!(EfiSystemTable, boot_services) == 96);
 
-const EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID: EfiGuid = EfiGuid {
-    data1: 0x9042a9de,
-    data2: 0x23dc,
-    data3: 0x4a38,
-    data4: [0x96, 0xfb, 0x7a, 0xde, 0xd0, 0x80, 0x51, 0x6a],
-}
-
-#[repr(C)]
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-struct EfiGuid {
-    data1: u32,
-    data2: u16,
-    data3: u16,
-    data4: [u8; 8],
-}
-
 #[repr(C)]
 #[derive(Debug)]
-struct EfiGraphicsOutputProtocol<'a> {
-    reserved: [u64; 3],
-    pub mode: &'a EfiGraphicsOutputProtocolMode<'a>,
+struct EfiGraphicsOutputProtocolPixelInfo {
+    version: u32,
+    pub horizontal_resolution: u32,
+    pub vertical_resolution: u32,
+    _padding0: [u32; 5],
+    pub pixels_per_scan_line: u32,
 }
+const _: () = assert!(size_of::<EfiGraphicsOutputProtocolPixelInfo>() == 36);
 
 #[repr(C)]
 #[derive(Debug)]
@@ -70,14 +77,10 @@ struct EfiGraphicsOutputProtocolMode<'a> {
 
 #[repr(C)]
 #[derive(Debug)]
-struct EfiGraphicsOutputProtocolPixelInfo {
-    version: u32,
-    pub horizontal_resolution: u32,
-    pub vertical_resolution: u32,
-    _padding0: [u32; 5],
-    pub pixels_per_scan_line: u32,
+struct EfiGraphicsOutputProtocol<'a> {
+    reserved: [u64; 3],
+    pub mode: &'a EfiGraphicsOutputProtocolMode<'a>,
 }
-const _: () = assert!(size_of::<EfiGraphicsOutputProtocolPixelInfo>() == 36);
 
 fn locate_graphics_protocol<'a>(
     efi_system_table: &EfiSystemTable
@@ -95,21 +98,19 @@ fn locate_graphics_protocol<'a>(
     Ok(unsafe { &*graphic_output_protocol })
 }
 
-use core::mem:offset_of;
-use core::mem::size_of;
-use core::panic::PanicInfo;
-use core::ptr::null_mut;
-use core::slice;
-
-type EfiVoid = u8;
-type EfiHandle = u64;
-type Result<T> = core::result::Result<T, &'static str>;
-
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
-#[must_use]
-#[repr(u64)]
-enum EfiStatus {
-    Success = 0,
+#[no_mangle]
+fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
+    let efi_graphics_output_protocol = locate_graphics_protocol(efi_system_table).unwrap();
+    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
+    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
+    let vram = unsafe {
+        slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
+    };
+    for e in vram {
+        *e = 0xffffff;
+    }
+    // println!("Hello, world!");
+    loop {}
 }
 
 #[panic_handler]
