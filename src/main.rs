bc544912c0c72c72e5cc1fd54900201b608f02b0
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-12-05 09:14:00 +0900

    p63

--- src/main.rs
+++ src/main.rs
@@ -79,6 +79,22 @@ struct EfiGraphicsOutputProtocolPixelInfo {
 }
 const _: () = assert!(size_of::<EfiGraphicsOutputProtocolPixelInfo>() == 36);
 
+fn locate_graphics_protocol<'a>(
+    efi_system_table: &EfiSystemTable
+) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
+    let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
+    let status = (efi_system_table.boot_services.locate_protocol) {
+        &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
+        null_mut::<EfiVoid>(),
+        &mut graphic_output_protocol as *mut *mut EfiGraphicsOutputProtocol
+        as *mut *mut EfiVoid,
+    }
+    if status != EfiStatus::Success {
+        return Err("Failed to locate graphics output protocol");
+    }
+    Ok(unsafe { &*graphic_output_protocol })
+}
+
 use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
